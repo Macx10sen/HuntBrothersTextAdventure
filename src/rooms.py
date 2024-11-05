da5459a8ebf4a8f3a09af64788796dc1ef40e8ba
Marcus <marchristensen@gmail.com>
2024-11-05 13:48:20 -0700

    Added code to rooms.py to declare class and create some generic rooms. This is not final structure, just a pitch for possible format.

--- src/rooms.py
+++ src/rooms.py
@@ -1,1 +1,26 @@
-#Declare Room Classes here
+#rooms
+
+class Room: #create Room class
+  def __init__(self, name, dsc): #define room
+    self.name 		= name
+    self.dsc 		= dsc
+    self.exits		= {} #dictionary for exits, as the direction defines also the room that the direction will take you to
+
+  def __str__(self):
+    return self.name
+
+  def create_exit(self, direction, NextRoom) #define exit method
+    self.exits[direction] = NextRoom
+
+#Create Rooms
+Lobby 		= Room("Lobby","Generic Description that says you can either go north, south, east, or west")
+N_ofLobby	= Room("NorthHall", "Generic Room Description")
+S_ofLobby	= Room("SouthHall", "Generic Room Description")
+W_ofLobby	= Room("WestHall", "Generic Room Description")
+E_ofLobby	= Room("WestHall", "Generic Room Description")
+
+#Create Exits in Rooms
+Lobby.create_exit("north", N_ofLobby)
+Lobby.create_exit("south", S_ofLobby)
+Lobby.create_exit("west", W_ofLobby)
+Lobby.create_exit("east", E_ofLobby)
