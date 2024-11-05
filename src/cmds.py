518fc301e1596c2fb59c806092879f5ff4eeca53
Marcus <marchristensen@gmail.com>
2024-11-05 13:30:27 -0700

    Added cmdlist to file cmd.py

--- src/cmds.py
+++ src/cmds.py
@@ -1,1 +1,10 @@
 #all commands will be in this file
+
+#cmd list
+Cmds = ["move [direction]", "Check [Stat]", "Grab [item]", "Store [item]", "Eat [item]", "help", "help [cmd]" ]  
+     #Move Options     : North, East, South, West; Depending on what room the character is located in. Use "help move" to see options
+     #Check Options    : Health, Hunger?, Inventory (inventory sub types?). Use "help check" to see options
+     #Grab Options     : Limited by items in the room the character is located in. Use "help grab" to see options
+     #Store Options    : Limited by items currently in the characters hands. Use "help store" to see options. Also "Check Inventory Hands"?
+     #Eat Options      : Limited by food items in hand. use for hunger bar? or health regen? Item Subtype?
+     #help shortcuts, help shortcuts[cmd]? show acceptable shorthand for various commands? specific commands? both?
