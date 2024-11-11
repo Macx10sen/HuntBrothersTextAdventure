8e29f160e2cf584b1b472664bdba9a1972d1ce53
DESKTOP-V3S0E57\Jacob <mattkangas1@gmail.com>
2024-11-10 18:46:42 -0700

    game loop draft

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from src.read_map_data import get_rooms
 
-name = input('Input your name: ')
-print(f'Hello {name}')
+rooms = get_rooms()
+print(rooms)
+
+current_room = rooms['Entrance Hall']
+
+while True:
+    print(current_room.name)
+    print(current_room.description)
+    print(current_room.exits)
+    command = input('Input your command: ')
+    print(f'You input {command}')
+    #current_room = rooms[current_room.exits['north']]
+    current_room = rooms['Library']
+
--- src/read_map_data.py
+++ src/read_map_data.py
@@ -1,13 +1,16 @@
+import os
 import yaml
 # import sys, os
 # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
-from rooms import Room
+from src.rooms import Room
 
-with open('bin/map_data/map1.yaml', 'r') as map_file:
-    room_data = yaml.load(map_file, Loader=yaml.SafeLoader)
-rooms = {}
-for room in room_data['rooms']:
-    name = room['name']
-    description = room['description']
-    rooms[name] = Room(name, description)
-print(rooms)
+def get_rooms():
+    map_file_path = os.path.join("bin", "map_data", "map1.yaml")
+    with open(map_file_path, 'r') as map_file:
+        room_data = yaml.load(map_file, Loader=yaml.SafeLoader)
+    rooms = {}
+    for room in room_data['rooms']:
+        name = room['name']
+        description = room['description']
+        rooms[name] = Room(name, description)
+    return rooms
