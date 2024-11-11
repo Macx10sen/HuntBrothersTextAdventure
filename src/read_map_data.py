ace7a1482fc46f9d0bda478600560b343a8f1291
Kolter <wksaurey@gmail.com>
2024-11-11 15:57:54 -0700

    starting to include connections class

--- src/read_map_data.py
+++ src/read_map_data.py
@@ -1,20 +1,30 @@
-import os
+import os, sys
 import yaml
 # import sys, os
 # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
-from .rooms import Room
 
 def get_rooms():
     map_file_path = os.path.join("bin", "map_data", "map1.yaml")
     with open(map_file_path, 'r') as map_file:
-        room_data = yaml.load(map_file, Loader=yaml.SafeLoader)
+        map_data = yaml.load(map_file, Loader=yaml.SafeLoader)
+        room_data = map_data['rooms']
+        connection_data = map_data['connections']
     rooms = {}
-    for room in room_data['rooms']:
+    for room in room_data:
         name = room['name']
         description = room['description']
-        exits = room['exits']
+        exits = get_connections(connection_data)
+        exits = {}
         rooms[name] = Room(name, description, exits)
     return rooms
 
+def get_connections(connection_data):
+    for connection in connection_data:
+        print(connection)
+
 if __name__ == '__main__':
+    from rooms import Room
     get_rooms()
+
+else:
+    from .rooms import Room
