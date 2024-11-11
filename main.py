7a0a3e140b249f093b45fab4686f3796558d7eab
Kolter <wksaurey@gmail.com>
2024-11-11 10:50:52 -0700

    included verbose room description method

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from src.read_map_data import get_rooms
 
 rooms = get_rooms()
-print(rooms)
-
 current_room = rooms['Entrance Hall']
 
 while True:
-    print(current_room.name)
-    print(current_room.description)
-    print(current_room.exits)
+    current_room.verbose()
     command = input('Input your command: ')
     print(f'You input {command}')
     current_room = rooms[current_room.exits['north']]
--- src/rooms.py
+++ src/rooms.py
@@ -8,3 +8,18 @@ class Room: #create Room class
 
   def __str__(self):
     return self.name
+
+  def verbose(self):
+    print(f'You are in the {self.name}. ', end='')
+    exit_directions = list(self.exits.keys())
+
+    if not exit_directions:
+      print('There are no visible exits.')
+    elif len(exit_directions) == 1:
+      print(f'You see an exit to the {exit_directions[0]}.')
+    elif len(exit_directions) == 2:
+      print(f'You see exits to the {exit_directions[0]} and {exit_directions[1]}.')
+    else:
+      print(f'You see exits to the {", ".join(exit_directions[:-1])}, and {exit_directions[-1]}.')
+
+    print(f'When you look around you see {self.description}')
