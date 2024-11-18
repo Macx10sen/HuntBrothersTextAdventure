a92b7dc80b647de0f09b2291186d3bf6f197ccdf
Marcus <marchristensen@gmail.com>
2024-11-17 22:04:08 -0700

    Added introduction message and player name query. input/response to name query creates player class

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from src.read_map_data import get_rooms
 from src.cmds import input_parsing
+from src.player import Player
+print(f'\nHunt Brothers Text Adventures Presents:\nEscape Room!\n')
+player_name = input("Welcome to the Escape Room! Please enter your name.")
 
+player = Player(player_name)
 current_room = get_rooms()
 
 while True:
