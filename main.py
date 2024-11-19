671380e8db86f28b57c16638b9034b4c855eeccf
Marcus <marchristensen@gmail.com>
2024-11-18 23:11:03 -0700

    Added a return after the name query to start a new line with an arrow

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.read_map_data import get_rooms
 from src.cmds import input_parsing
 from src.player import Player
 print(f'\nHunt Brothers Text Adventures Presents:\nEscape Room!')
-player_name = input(f"\nWelcome to the Escape Room! Please enter your name.")
+player_name = input(f'\nWelcome to the Escape Room! Please enter your name.\n--> ')
 
 player = Player(player_name)
 player.current_room = get_rooms()
