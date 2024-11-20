d45330bc050d869007748588d1ff8cff266dfe13
Marcus <marchristensen@gmail.com>
2024-11-19 17:33:57 -0700

    Created command synonyms and added functions to execute code based on methods to clean up input_parsing function

--- src/cmds.py
+++ src/cmds.py
@@ -1,31 +1,53 @@
 #all commands will be in this file
 
+def move(player, cmd):
+    new_room, error_message = player.current_room.move(cmd)
+    if new_room == None:
+        print(error_message)
+    else:
+        player.current_room = new_room
+
+def check(player, cmd):
+    if cmd == "inventory":
+        print(player.display_inv())
+    elif cmd == "room":
+        player.display_room()
+
+def grab(player, item):
+    player.add_to_inv(player, item)
+
+#cmd synonyms -- add to or change these as needed
+move_syn = ["move", "m", "go", "g", "travel", "walk", "run"]
+check_syn = ["check", "c"]
+help_syn = ["help", "h"]
+grab_syn = ["grab", "g", "obtain", "pick"] #pick?
+
 #cmd list
-Cmds = ["move [direction]", "Check [Stat]", "Grab [item]", "Store [item]", "Eat [item]", "help", "help [cmd]" ]  
+
+'''Cmds = ["move [direction]", "Check [Stat]", "Grab [item]", "Store [item]", "Eat [item]", "help", "help [cmd]" ]  
      #Move Options     : North, East, South, West; Depending on what room the character is located in. Use "help move" to see options
      #Check Options    : Health, Hunger?, Inventory (inventory sub types?). Use "help check" to see options
      #Grab Options     : Limited by items in the room the character is located in. Use "help grab" to see options
      #Store Options    : Limited by items currently in the characters hands. Use "help store" to see options. Also "Check Inventory Hands"?
      #Eat Options      : Limited by food items in hand. use for hunger bar? or health regen? Item Subtype?
-     #help shortcuts, help shortcuts[cmd]? show acceptable shorthand for various commands? specific commands? both?
+     #help shortcuts, help shortcuts[cmd]? show acceptable shorthand for various commands? specific commands? both?'''
+
 
 def input_parsing(player,cmd):
+#    cmd_list = [
+#        {move_syn:  move()},
+#        {check_syn: check()},
+#        {grab_syn:  grab()}
+#    ]
     cmd                     = cmd.lower().split()
     primary_cmd             = cmd[0]
     secondary_cmds          = cmd[1:]
-    if primary_cmd == "move" or primary_cmd == "m":
-        new_room, error_message = player.current_room.move(secondary_cmds[0])
-        if new_room == None:
-            print(error_message)
-        else:
-            player.current_room = new_room
-    elif primary_cmd == "check" or primary_cmd == "c":
-        if secondary_cmds[0] == "inventory":
-            print(player.display_inv())
-        elif secondary_cmds[0] == "room":
-            player.display_room()
-    elif primary_cmd == "grab" or primary_cmd == "g":
-        player.add_to_inv(secondary_cmds[0])
+    if primary_cmd in move_syn:
+        move(player, secondary_cmds[0])
+    elif primary_cmd in check_syn:
+        check(player, secondary_cmds[0])
+    elif primary_cmd in grab_syn:
+        grab(player, secondary_cmds[0])
     else:
         print(f"{player.name}, that is not a valid command. Please try again")
 
