b307727b7708330bdc954b09b55965dfbe32e467
Marcus <marchristensen@gmail.com>
2024-11-19 21:13:40 -0700

    moved room display outside of gameloop to be called once before the gameloop starts;

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ player_name = input(f'\nWelcome to the Escape Room! Please enter your name.\n-->
 player = Player(player_name)
 player.current_room = get_rooms()
 
+player.display_room() #display room before beginning loop, comment this line to put in loop
+
 while True:
-    player.display_room()
+#    player.display_room() #comment this line to remove from loop
     command = input('--> ')
     input_parsing(player,command)
 
