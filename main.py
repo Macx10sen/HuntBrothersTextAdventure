ca465aa226814aad8f0fb662c76a2dd6aa8ec63f
Kolter <wksaurey@gmail.com>
2024-11-22 11:22:26 -0700

    cleaned up main.py

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 from src.read_map_data import get_rooms
 from src.cmds import input_parsing
 from src.player import Player
-print(f'\nHunt Brothers Text Adventures Presents:\nEscape Room!')
-player_name = input(f'\nWelcome to the Escape Room! Please enter your name.\n--> ')
-print()
 
-player = Player(player_name)
-player.current_room = get_rooms()
+def init():
+    print(f'\nHunt Brothers Text Adventures Presents:\nEscape Room!')
+    player_name = input(f'\nWelcome to the Escape Room! Please enter your name.\n--> ')
+    print()
 
-player.display_room() #display room before beginning loop, comment this line to put in loop
+    player = Player(player_name)
+    player.current_room = get_rooms()
 
-while True:
-#    player.display_room() #comment this line to remove from loop
-    command = input('\n--> ')
-    input_parsing(player, command)
+    player.display_room() #display room before beginning loop, comment this line to put in loop
+    
+    gameloop(player)
 
-'''
-gameloop:
-    print room 
-        name
-        description
-        exits
-        items
-    get user input
-    process command
-'''
+def gameloop(player):
+    while True:
+    #    player.display_room() #comment this line to remove from loop
+        command = input('\n--> ')
+        input_parsing(player, command)
+
+init()
